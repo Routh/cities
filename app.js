893de0d06f1b70cba6299d73f976989f54240ce9
routh <routhy@gmail.com>
2013-08-05 12:54:28 -0400

    Changed JSON.Parse Type.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var locations = JSON.parse(fs.readFileSync(__dirname + '/data/world-cities.json'));
+var locations = JSON.parse(fs.readFileSync(__dirname + '/data/world-cities.json.gz'));
 
 var R = 6371;
 var haversine = function(lat1, lon1, lat2, lon2)
